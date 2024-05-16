da85c38c107c9c520efa7d3e2e34715cf5ffe5a7
UmgefallenesGlas <108078099+UmgefallenesGlas@users.noreply.github.com>
2024-05-16 07:51:06 +0200

    Game.h comments

--- Raylib_ECS/Game.h
+++ Raylib_ECS/Game.h
@@ -6,12 +6,13 @@
 class Game
 {
 public:
-	Game();
-	~Game();
-
+	Game();//Doesnt need this, should i delete it?
+	~Game();//This too
+	
 	void Init();
 	void Update();
 	void Draw();
+	//Need to add Unload()
 	//Declaration for Manager here. Also want to have a refrence to Manager for my Components
 	static Camera2D camera;
 
