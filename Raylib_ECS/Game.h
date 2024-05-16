a2962d64fe9220ce4220387f9d4dc47b0a22f856
UmgefallenesGlas <108078099+UmgefallenesGlas@users.noreply.github.com>
2024-05-16 07:47:19 +0200

    Game.h comments
    Addet comment what to do

--- Raylib_ECS/Game.h
+++ Raylib_ECS/Game.h
@@ -12,7 +12,7 @@ public:
 	void Init();
 	void Update();
 	void Draw();
-
+	//Declaration for Manager here. Also want to have a refrence to Manager for my Components
 	static Camera2D camera;
 
 	enum groupLabels : std::size_t {
